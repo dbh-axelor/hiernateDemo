9c34c782476471321e97fd6415c9f4606e9761df
dbh-axelor <d.bhuva@axelor.com>
2024-01-17 18:46:54 +0530

    added Student Service to manage crud operation(create,read)

--- hibernate-jpa-demo/src/main/java/org/example/Main.java
+++ hibernate-jpa-demo/src/main/java/org/example/Main.java
@@ -22,136 +22,28 @@ import org.example.StudentService;
 
 public class Main {
 	
-	private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Demo-jpa");
 	
-	private static EntityManager entityManager = entityManagerFactory.createEntityManager();
 
     private StudentService studentService = new StudentService();
     
     public static void main(String[] args) {
     	
+    	String clgname = "VNSGU";
+    	String studentName = "Dishant";
+    	String studentAddr = "Surat";
+    	String studentDepart = "IT";
+    	long studentPh = 1111122222;
+    	String ex1 = "java";
+    	String ex2 = "Web Development";
+    	String studentCourse = "java Backend Developer";
     	
     	System.out.println("sucesss ----------------->>>>>>>>>> ");
-    	
-    	StudentService.hello("devang");
+//    	StudentService.createStudent(clgname,studentName,studentAddr, studentDepart,studentPh, ex1, ex2, studentCourse);
 
-    	Main objMain = new Main();
-    	
-//    	objMain.createStudent();
-    	
-    	objMain.showStudent();
+    	StudentService.showData();
 
     }
 
 
-	private void showStudent() {
-		try {		
-			List<Student> students = entityManager.createQuery("FROM Student",Student.class).getResultList();
-			
 
-			
-			for(Student records : students) {
-				System.out.println(records.getStidId());
-				System.out.println(records.getStudName());
-				
-			}
-			
-	
-			entityManager.close();
-		} catch (Exception e) {
-		e.printStackTrace();
-		}
-		
-	}
-
-
-	private void createStudent() {
-        EntityTransaction tx = entityManager.getTransaction();
-       
-		try {
-			tx.begin();
-			
-			// add college
-	      
-			College clg = new College();
-			clg.setCollegeName("utu");
-			
-			entityManager.persist(clg);
-
-			
-			
-			// add student
-			
-			Student student = new Student();
-//			student.setStudId(1);
-			student.setStudName("devang");
-			student.setStudAdd("surat");
-			student.setStuddepartment("IT");
-			
-			entityManager.persist(student);
-			
-			student.setCollege(clg); 
-			
-			// create phone 
-			Phone phone = new Phone();
-			phone.setPhNumber(1111222333);
-			phone.setStudent(student);
-			
-			student.setPhone(phone);
-			entityManager.persist(phone);
-			
-			// add exam
-			
-			Exam exam1 = new Exam();
-			exam1.setStudent(student);
-			exam1.setSubject("java");
-			
-			Exam exam2 = new Exam();
-			exam2.setStudent(student);
-			exam2.setSubject("python");
-			
-			List<Exam> examList = new ArrayList<Exam>();
-			examList.add(exam1);
-			examList.add(exam2);
-			
-			student.setExams(examList);
-			
-			// add course 
-			
-			Course course = new Course();
-			course.setCourseName("Java Backend Developer");
-			
-			List<Course> courseList = new ArrayList<Course>();
-			courseList.add(course);
-			
-			List<Student> studentList = new ArrayList<Student>();
-			studentList.add(student);
-			
-			clg.setStudents(studentList);
-			
-			student.setCourses(courseList);
-
-			course.setStudents(studentList);
-			
-			
-			entityManager.persist(course);
-			
-
-
-			
-
-			
-			
-			
-			tx.commit();
-		} catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            entityManager.close();
-            entityManagerFactory.close();
-        }
-    }
 }
--- hibernate-jpa-demo/src/main/java/org/example/StudentService.java
+++ hibernate-jpa-demo/src/main/java/org/example/StudentService.java
@@ -1,20 +1,113 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.example.entities.College;
+import org.example.entities.Course;
+import org.example.entities.Exam;
+import org.example.entities.Phone;
+import org.example.entities.Student;
+
 public class StudentService {
 	
+	public static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Demo-jpa");
+	
+	public static EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
-	public static void hello(String name) {
-		try {
-			System.out.println("hii this is "+name+" from main class");			
+	public static void showData() {
+			try {		
+			List<Student> students = entityManager.createQuery("FROM Student",Student.class).getResultList();
 			
+	
+			
+			for(Student records : students) {
+				System.out.println("Student ID : "+records.getStidId());
+				System.out.println("Student Name : "+records.getStudName());
+				System.out.println();
+				
+			}
+			
+	
+			entityManager.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static void createStudent() {
-		
-		
-	}
+	public static void createStudent(String clgname,String studentName,String studentAddr, String studentDepart, long studentPh, String ex1, String ex2, String studentCourse) {
+		EntityTransaction tx = entityManager.getTransaction();
+	       
+		try {
+			tx.begin();
+			
+			// add college
+			College clg = new College();
+			clg.setCollegeName(clgname);		// college name
+			entityManager.persist(clg);
+
+			// add student
+			Student student = new Student();
+			student.setStudName(studentName); 			// student name
+			student.setStudAdd(studentAddr);			// student address
+			student.setStuddepartment(studentDepart);	// student department
+			
+			entityManager.persist(student);
+			
+			student.setCollege(clg); 
+			
+			// create phone 
+			Phone phone = new Phone();
+			phone.setPhNumber(studentPh);				// student phone number
+			phone.setStudent(student);
+			
+			student.setPhone(phone);
+			entityManager.persist(phone);
+			
+			// add exam
+			
+			Exam exam1 = new Exam();
+			exam1.setStudent(student);
+			exam1.setSubject(ex1);						// exam name 1
+			
+			Exam exam2 = new Exam();
+			exam2.setStudent(student);
+			exam2.setSubject(ex2);						// exam name 2
+			
+			List<Exam> examList = new ArrayList<Exam>();
+			examList.add(exam1);
+			examList.add(exam2);
+			
+			student.setExams(examList);
+			
+			// add course 
+			Course course = new Course();
+			course.setCourseName(studentCourse); 	   // course name
+			
+			List<Course> courseList = new ArrayList<Course>();
+			courseList.add(course);
+			List<Student> studentList = new ArrayList<Student>();
+			studentList.add(student);
+			clg.setStudents(studentList);
+			student.setCourses(courseList);
+			course.setStudents(studentList);
+			entityManager.persist(course);
+			
+			tx.commit();
+		} catch (Exception e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
 	
 }
